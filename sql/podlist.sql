a6d296bd5a6faab5965e700e1e902c7e3b958e55
Huseyin Eltutan <heltutan54@gmail.com>
2019-11-23 22:05:44 +0100

    Modify small spellings

--- sql/podlist.sql
+++ sql/podlist.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS podlist;
 CREATE TABLE podlist (
 	id SERIAL primary key,
 	podlist_id VARCHAR(255),
-  title VARCHAR(255),
+  	title VARCHAR(255),
 	star_num INTEGER default 0,
 	img_url VARCHAR(300),
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
